5463c2e770cdaad85ce509804a00ba776eb730bf
lukasz4444 <93074713+lukasz4444@users.noreply.github.com>
2022-06-12 12:45:44 +0200

    0.0.2 Szybka poprawa błędu.
    Od teraz program będzie mówił że wyłączono wrota jeśli rzeczywiście się to uda.

--- polska wersja/off.lua
+++ polska wersja/off.lua
@@ -1,11 +1,14 @@
 os = require("os")
+term = require("term")
 c = require("component")
 sg = c.stargate
+term.clear()
 sg.abortDialing()
 local g,d = sg.disengageGate()
-
 if d == "stargate_failure_wrong_end" then
     print("Nie można wyłączyć przychodzącego tunelu")
 elseif d == "stargate_failure_not_open" then
     print("Wrota nie są otwarte.")
+elseif g == "stargate_disengage" then
+    print("Wrota pomyślnie wyłączono.")
 end
